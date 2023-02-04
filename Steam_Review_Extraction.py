db04ef7d63bdc803d9d657d42f7daa0abf4441b7
Sven-XinyuDong <Sven_XinyuDong@outlook.com>
2023-02-04 11:44:35 -0800

    branch update

--- Steam_Review_Extraction.py
+++ Steam_Review_Extraction.py
@@ -1,10 +1,28 @@
+<<<<<<< Updated upstream:Steam_Review_Extraction.py
 ### Dependencies
+=======
+# !/usr/bin/python
+#
+# Copyright (C) 2023 Xinyu Dong, Mattew Yau.
+# You may obtain a copy of the License at
+#
+# https://github.com/Sven-XinYuDong/SteamSMART/blob/main/LICENSE.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#### Dependencies
+>>>>>>> Stashed changes:APICall.py
 import requests
 import pandas as pd
 import json
 import re
+import numpy as np
 
-### Run this code block to change how many reviews we want to get
+#### Get game reviews from Steam
 def get_reviews(appid, params={'json':1}):
     '''
     Get game reveiw data from steampowered API in JSON format
@@ -58,3 +76,121 @@ def review_cleaning(df):
     df['review'] = df['review'].map(rt) # remove punctuation and numbers
     df['review'] = df['review'].str.lower() #all lower cased
     return df
+
+
+def set_key(user_key):
+    '''
+    global key for Steam API
+    '''
+    global key
+    key = user_key
+
+
+##### get_friend_list(steamid)
+def get_friend_list(steam_id):
+    '''
+    Get the friend list given a steam_id,return a DataFrame of friends's steamid
+
+    Parameters:
+        steam_id - the Steam ID to look up
+
+    Returns:
+        A DataFrame with friends'id, relationship and being friends since when
+    '''
+
+    #Formulate url
+    url_friends_prefix = " http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="
+    url_tail = '&relationship=friend'
+    url = url_friends_prefix+key+'&steamid='+steam_id+url_tail
+
+    response = requests.get(url)  #Get response
+
+    #Return pandas DataFrame
+    friends = json.loads(response.text)
+    friends = pd.DataFrame(friends['friendslist']['friends'])
+    return friends
+
+
+#### get_games_owned(steam_id)
+def get_games_owned(steam_id):
+    '''
+    Get the games details owned by given Steam ID
+
+    Parameters:
+        steam_id - the id to to specify player
+
+    Returns:
+        A DataFrame with appid, game name, playtime and other related details of owned games
+
+    '''
+    #Formulate url
+    url_owneGames_prefix = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
+    url_ownGames_tail = "&include_appinfo=1&format=json"
+    url = url_owneGames_prefix+key+'&steamid='+steam_id+url_ownGames_tail
+
+    response = requests.get(url)  #Get response
+
+    #Return pandas DataFrame
+    games = json.loads(response.text)
+    games = pd.DataFrame(games['response']['games'])
+    return games
+
+
+#### get_game_genre(app_id)
+def get_game_genres(app_id):
+    '''
+    Get the genre infomation of an given app
+
+    Parameters:
+        app_id: the id of games in Steam Store
+
+    Returns:
+        A list of genres description for the App
+    '''
+    #Formulate url
+    url_gamedetail_prefix = "https://store.steampowered.com/api/appdetails/?appids="
+    url_gamedetail_tail = "&cc=EE&l=english&v=1 HTTP/1.1" "-" "Valve/Steam HTTP Client 1.0 (tenfoot)"
+    url = url_gamedetail_prefix+app_id+url_gamedetail_tail
+
+    response = requests.get(url)  #Get response
+
+    #Return a list of genres tags
+    try:
+        detail = requests.get(url).json()[app_id]["data"]
+        detail = pd.DataFrame(detail['genres'])
+        genres = list(detail.description)
+        return genres
+
+    except:
+        return ['NA']
+
+
+#### compare_genres_preference(steam_id_list)
+def compare_genres_preference(steam_id_list):
+    '''
+    Provide a summary table of the genres of games owned by user in a list of Steam IDs.
+
+    Parameters:
+        steam_id_list - a list of Steam IDs
+
+    Returns:
+        A Dataframe with Steam IDs and correspondent genres belonging to games they own.
+    '''
+
+    id_genres_df = pd.DataFrame()
+
+    for steam_id in steam_id_list:
+        games_owned = get_games_owned(steam_id)
+        #Add genres infomation to game DataFrame
+        games_owned['genres'] = games_owned['appid'].apply(str).apply(get_game_genres)
+
+        #unnest genres into long Dataframe for Viz
+        long_tags = []
+        for tag in games_owned['genres']:
+            long_tags += tag
+        d = {'steamid': list(np.repeat(steam_id,len(long_tags))),
+             'genres':long_tags }
+        df = pd.DataFrame(d)
+        id_genres_df = pd.concat([id_genres_df,df])
+
+    return id_genres_df
